2dd8770f9755e46d8a1c89b237c891419a7c1c28
Roman Filatov <ikardoc@gmail.com>
2022-05-27 22:23:23 +0300

    Base code from 17.6.4

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Account
+{
+    // тип учетной записи
+    public string Type { get; set; }
+
+    // баланс учетной записи
+    public double Balance { get; set; }
+
+    // процентная ставка
+    public double Interest { get; set; }
+}
+
+public static class Calculator
+{
+    // Метод для расчета процентной ставки
+    public static void CalculateInterest(Account account)
+    {
+        if (account.Type == "Обычный")
+        {
+            // расчет процентной ставки обычного аккаунта по правилам банка
+            account.Interest = account.Balance * 0.4;
+
+            if (account.Balance < 1000)
+                account.Interest -= account.Balance * 0.2;
+
+            if (account.Balance < 50000)
+                account.Interest -= account.Balance * 0.4;
+        }
+        else if (account.Type == "Зарплатный")
+        {
+            // расчет процентной ставк зарплатного аккаунта по правилам банка
+            account.Interest = account.Balance * 0.5;
+        }
+    }
+}
